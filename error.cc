3a8b48914c06303baffbfa9290b1430a5237a6ea
Kuratius <Kuratius@gmx.net>
2025-08-19 02:44:21 +0200

    remove unnecessary buffer again

--- error.cc
+++ error.cc
@@ -13,20 +13,26 @@
 
 error::error(const char* str)
 {
-    char temp[129];
-    temp[128]='\0';
-	if(strlen(str)>128)
+    //char temp[129];
+    //temp[128]='\0';
+    int len=strlen(str);
+	if(len>128)
     {
 		//str[128]='\0';
         printf("FATAL ERROR! STRING LENGTH EXCEEDS BUFFER SIZE \n");
+        return;
     }
-
-    for(int i=0; i<128;i++){
+/*
+    for(int i=0; i<len;i++){
         temp[i]=str[i];
     }
+*/
+
+//    temp[len]='\0';
 
 
-	strcpy(this->str,temp);
+	strcpy(this->str,str);
+
 }
 
 void error::debug(const char* str,long num)
